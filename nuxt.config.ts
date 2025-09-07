e224f19a92c3137c79678821c2453c7ac7d8f8b5
Felix Rizzolli <felix.rizzolli@outlook.de>
2025-09-07 13:47:54 +0000

    feat: adjust logo color and add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,24 @@ export default defineNuxtConfig({
         ],
     },
 
+    app: {
+        head: {
+            link: [
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+                { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+                { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+                { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon-180x180.png' },
+                { rel: 'apple-touch-icon', sizes: '167x167', href: '/apple-touch-icon-167x167.png' },
+                { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#000000' },
+                { rel: 'manifest', href: '/site.webmanifest' },
+            ],
+            meta: [
+                { name: 'theme-color', content: '#efb100' },
+                { name: 'msapplication-TileColor', content: '#efb100' },
+            ],
+        },
+    },
+
     svgo: {
         componentPrefix: 'i',
         autoImportPath: '~/assets/images/',
